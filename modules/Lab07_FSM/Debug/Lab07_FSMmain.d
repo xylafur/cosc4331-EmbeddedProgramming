947c1ba38c636960527055f4da28c943078e99bd
Kesley Richardson <james@jkesley.com>
2019-03-02 13:02:38 -0600

    Adding code from class

---
+++ modules/Lab07_FSM/Debug/Lab07_FSMmain.d
@@ -0,0 +1,41 @@
+# FIXED
+
+Lab07_FSMmain.obj: ../Lab07_FSMmain.c
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/ccs_base/arm/include/msp.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/ccs_base/arm/include/msp432p401r.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/ccs_base/arm/include/msp_compatibility.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/ccs_base/arm/include/msp432p401r_classic.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+Lab07_FSMmain.obj: /opt/ccstudio/ccsv8/ccs_base/arm/include/system_msp432p401r.h
+Lab07_FSMmain.obj: ../../inc/Clock.h
+Lab07_FSMmain.obj: ../../inc/LaunchPad.h
+Lab07_FSMmain.obj: ../../inc/TExaS.h
+
+../Lab07_FSMmain.c: 
+/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h: 
+/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h: 
+/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h: 
+/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h: 
+/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h: 
+/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h: 
+/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h: 
+/opt/ccstudio/ccsv8/ccs_base/arm/include/msp.h: 
+/opt/ccstudio/ccsv8/ccs_base/arm/include/msp432p401r.h: 
+/opt/ccstudio/ccsv8/ccs_base/arm/include/msp_compatibility.h: 
+/opt/ccstudio/ccsv8/ccs_base/arm/include/msp432p401r_classic.h: 
+/opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h: 
+/opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+/opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+/opt/ccstudio/ccsv8/ccs_base/arm/include/system_msp432p401r.h: 
+../../inc/Clock.h: 
+../../inc/LaunchPad.h: 
+../../inc/TExaS.h: 
