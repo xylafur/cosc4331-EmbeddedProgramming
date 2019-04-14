e89e2fc472dd06329155d3c4f97ca64950af8cd6
xylafur <jkesleyrichardson@gmail.com>
2019-04-13 21:59:28 -0500

    Improvment to makefiles

--- modules/Lab13_Timers/Debug/subdir_rules.mk
+++ modules/Lab13_Timers/Debug/subdir_rules.mk
@@ -4,27 +4,13 @@
 
 INC_DIR = /home/yelsek/projects/cosc4331-EmbeddedProgramming/modules/inc
 
-# Each subdirectory must supply rules for building sources it contributes
-Bump.obj: $(INC_DIR)/Bump.c $(GEN_OPTS) | $(GEN_FILES)
+%.obj: $(INC_DIR)/%.c $(GEN_OPTS) | $(GEN_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: MSP432 Compiler'
 	"/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include" --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS" --include_path="/home/yelsek/projects/tirslk_maze_1_00_01/tirslk_maze_1_00_00/Lab13_Timers" --include_path="/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
-Clock.obj: $(INC_DIR)/Clock.c $(GEN_OPTS) | $(GEN_FILES)
-	@echo 'Building file: "$<"'
-	@echo 'Invoking: MSP432 Compiler'
-	"/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include" --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS" --include_path="/home/yelsek/projects/tirslk_maze_1_00_01/tirslk_maze_1_00_00/Lab13_Timers" --include_path="/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
-	@echo 'Finished building: "$<"'
-	@echo ' '
-
-CortexM.obj: $(INC_DIR)/CortexM.c $(GEN_OPTS) | $(GEN_FILES)
-	@echo 'Building file: "$<"'
-	@echo 'Invoking: MSP432 Compiler'
-	"/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include" --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS" --include_path="/home/yelsek/projects/tirslk_maze_1_00_01/tirslk_maze_1_00_00/Lab13_Timers" --include_path="/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
-	@echo 'Finished building: "$<"'
-	@echo ' '
 
 %.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES)
 	@echo 'Building file: "$<"'
@@ -32,40 +18,4 @@ CortexM.obj: $(INC_DIR)/CortexM.c $(GEN_OPTS) | $(GEN_FILES)
 	"/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include" --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS" --include_path="/home/yelsek/projects/tirslk_maze_1_00_01/tirslk_maze_1_00_00/Lab13_Timers" --include_path="/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
 	@echo 'Finished building: "$<"'
 	@echo ' '
-
-LaunchPad.obj: $(INC_DIR)/LaunchPad.c $(GEN_OPTS) | $(GEN_FILES)
-	@echo 'Building file: "$<"'
-	@echo 'Invoking: MSP432 Compiler'
-	"/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include" --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS" --include_path="/home/yelsek/projects/tirslk_maze_1_00_01/tirslk_maze_1_00_00/Lab13_Timers" --include_path="/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
-	@echo 'Finished building: "$<"'
-	@echo ' '
-
-Motor.obj: $(INC_DIR)/Motor.c $(GEN_OPTS) | $(GEN_FILES)
-	@echo 'Building file: "$<"'
-	@echo 'Invoking: MSP432 Compiler'
-	"/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include" --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS" --include_path="/home/yelsek/projects/tirslk_maze_1_00_01/tirslk_maze_1_00_00/Lab13_Timers" --include_path="/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
-	@echo 'Finished building: "$<"'
-	@echo ' '
-
-PWM.obj: $(INC_DIR)/PWM.c $(GEN_OPTS) | $(GEN_FILES)
-	@echo 'Building file: "$<"'
-	@echo 'Invoking: MSP432 Compiler'
-	"/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include" --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS" --include_path="/home/yelsek/projects/tirslk_maze_1_00_01/tirslk_maze_1_00_00/Lab13_Timers" --include_path="/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
-	@echo 'Finished building: "$<"'
-	@echo ' '
-
-TExaS.obj: $(INC_DIR)/TExaS.c $(GEN_OPTS) | $(GEN_FILES)
-	@echo 'Building file: "$<"'
-	@echo 'Invoking: MSP432 Compiler'
-	"/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include" --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS" --include_path="/home/yelsek/projects/tirslk_maze_1_00_01/tirslk_maze_1_00_00/Lab13_Timers" --include_path="/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
-	@echo 'Finished building: "$<"'
-	@echo ' '
-
-TimerA1.obj: $(INC_DIR)/TimerA1.c $(GEN_OPTS) | $(GEN_FILES)
-	@echo 'Building file: "$<"'
-	@echo 'Invoking: MSP432 Compiler'
-	"/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include" --include_path="/opt/ccstudio/ccsv8/ccs_base/arm/include/CMSIS" --include_path="/home/yelsek/projects/tirslk_maze_1_00_01/tirslk_maze_1_00_00/Lab13_Timers" --include_path="/opt/ccstudio/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
-	@echo 'Finished building: "$<"'
-	@echo ' '
-
 
